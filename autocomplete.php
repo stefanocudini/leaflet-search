1819e34cef85dbd5c67c6aea21209790b4cc4998
z4k <none@none>
2012-08-09 12:24:35 +0200

    added comments into autocomplete.php

--- autocomplete.php
+++ autocomplete.php
@@ -10,7 +10,7 @@
 
 @header("Content-type: text/plain; charset=utf-8");
 
-$coords = json_decode('[
+$data = json_decode('[
 	{"loc":[41.239190,13.032145], "title":"black"},
 	{"loc":[41.807149,13.162994], "title":"blue"},
 	{"loc":[41.219190,13.062145], "title":"cyan"},
@@ -20,20 +20,20 @@ $coords = json_decode('[
 	{"loc":[41.794008,12.583884], "title":"green"},	
 	{"loc":[41.575730,13.002411], "title":"red"},	
 	{"loc":[41.546175,13.673590], "title":"yellow"}
-]',true);
+]',true);	//simulate few database data
 
 $qreg = $_GET['q'];
 $reg = "/^$qreg/i";	//initial search
 
-function searchInit($text)
+function searchInit($text)	//search initial text in titles
 {
 	global $reg;
 	return (bool)preg_match($reg, $text['title'],$m);
 }
 
-$fcoords = array_filter($coords,'searchInit');
+$fdata = array_filter($data,'searchInit');	//filter data by title
 
-$res = array('ok'=>1, 'results'=> $fcoords);
+$res = array('ok'=>1, 'results'=> $fdata);	//formatting json result
 
 echo json_encode($res);
 
