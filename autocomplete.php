b94209f1ba4148b4ace3f28b8ed91e8cde3c2772
z4k <none@none>
2012-08-09 12:21:28 +0200

    sort json data for example

--- autocomplete.php
+++ autocomplete.php
@@ -11,15 +11,15 @@
 @header("Content-type: text/plain; charset=utf-8");
 
 $coords = json_decode('[
-	{"loc":[41.575730,13.002411], "title":"red"},
-	{"loc":[41.807149,13.162994], "title":"blue"},
-	{"loc":[41.546175,13.673590], "title":"yellow"},
-	{"loc":[41.794008,12.583884], "title":"green"},
 	{"loc":[41.239190,13.032145], "title":"black"},
+	{"loc":[41.807149,13.162994], "title":"blue"},
 	{"loc":[41.219190,13.062145], "title":"cyan"},
-	{"loc":[41.319190,13.162145], "title":"gray"},	
+	{"loc":[41.344190,13.242145], "title":"darkblue"},	
 	{"loc":[41.679190,13.122145], "title":"darkred"},
-	{"loc":[41.344190,13.242145], "title":"darkblue"}
+	{"loc":[41.319190,13.162145], "title":"gray"},
+	{"loc":[41.794008,12.583884], "title":"green"},	
+	{"loc":[41.575730,13.002411], "title":"red"},	
+	{"loc":[41.546175,13.673590], "title":"yellow"}
 ]',true);
 
 $qreg = $_GET['q'];
