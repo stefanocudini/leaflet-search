4ba7dda4de34e6b605daa85c978a7e85f8e24933
Andreas B. Wagner <AndreasBWagner@pointfree.net>
2013-02-15 11:14:20 -0500

    Autoresize input field with autotype.

--- leaflet-search.js
+++ leaflet-search.js
@@ -263,12 +263,12 @@ L.Control.Search = L.Control.extend({
 	},
 
 	_autotype: function(e,prefix) {
-    // FIXME: autoresize for autotype
 		if (this.options.autotype && (this._tooltip.style.display != 'none') && (e.keyCode != 8) && (e.keyCode != 46)) { // Don't autotype after deleting.
 			var start = this._input.value.length;
 			var firstRecord = this._tooltip.getElementsByTagName('a')[0].innerHTML; // FIXME: find a way without innerHTML that also guarantees correct order (application developer may want images in tooltip)
 			var end = firstRecord.length;
 			this._input.value = firstRecord;
+			this._handleAutoresize();
 			if (this._input.createTextRange) {
 				var selRange = this._input.createTextRange();
 				selRange.collapse(true);
