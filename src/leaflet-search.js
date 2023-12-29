ac1ccbcfc5bde9e1bf16abd7a9002562f440c352
Chau Ping Lian <chauping@agmo.group>
2023-12-29 20:47:54 +0800

    Fix broken records cache from sourceData/JSONP/AJAX.

--- src/leaflet-search.js
+++ src/leaflet-search.js
@@ -733,7 +733,7 @@
         }
 
         this._curReq = this._retrieveData.call(this, inputText, function (data) {
-          self._recordsCache = self._formatData(self, data)
+          self._recordsCache = self._formatData.call(self, data)
 
           // TODO refact!
           if (self.options.sourceData) { records = self._filterData(self._input.value, self._recordsCache) } else { records = self._recordsCache }
